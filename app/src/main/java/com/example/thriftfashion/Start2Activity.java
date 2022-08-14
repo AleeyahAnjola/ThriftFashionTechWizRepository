b8d508106969bf470b1a883cfcc60158c9df6a8e
AleeyahAnjola <anjolaoluwami20@gmail.com>
2022-08-14 19:05:52 +0100

    First Commit

---
+++ app/src/main/java/com/example/thriftfashion/Start2Activity.java
@@ -0,0 +1,13 @@
+package com.example.thriftfashion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start2);
+    }
+}
